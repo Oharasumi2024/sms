e9df40e2b06b02b2181acb688547bb1de630e3b3
Oharasumi <ymg2465034@stu.o-hara.ac.jp>
2025-06-18 10:21:46 +0900

    角田作業

--- src/dao/TestListStudentDao.java
+++ src/dao/TestListStudentDao.java
@@ -7,12 +7,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import bean.School;
 import bean.Student;
+import bean.TestListStudent;
 
 public class TestListStudentDao extends Dao {
 
-	private String baseSql="select * from subject where ";
+	private String baseSql="select * from test where ";
 
 	private List<TestListStudent> postFilter(ResultSet rSet)throws Exception{
 		// リストを初期化
@@ -23,9 +23,8 @@ public class TestListStudentDao extends Dao {
 				// 学生リストインスタンスを初期化
 				TestListStudent testliststudent = new TestListStudent();
 				// 学生リストインスタンスに検索結果をセット
-				testliststudent.setSubjectName(rSet.getString("subjectName"));
-				testliststudent.setSubjectCd(rSet.getString("subjectCd"));
-				testliststudent.setNum(rSet.getInt("num"));
+				testliststudent.setSubjectCd(rSet.getString("subject_cd"));
+				testliststudent.setNum(rSet.getInt("no"));
 				testliststudent.setPoint(rSet.getInt("point"));
 				// リストに追加
 				list.add(testliststudent);
@@ -34,7 +33,6 @@ public class TestListStudentDao extends Dao {
 			e.printStackTrace();
 		}
 		return list;
-	}
 	}
 
 	public List<TestListStudent> filter(Student student)throws Exception{
@@ -47,18 +45,38 @@ public class TestListStudentDao extends Dao {
 		//リザルトセット
 		ResultSet rSet=null;
 		//条件
-		String condition="school_cd=? ";
+		String condition="student_no=? ";
 		//ソート
-		String order="order by cd";
+		String order="order by subject_cd";
 		try {
 			//プリパレにステートメントをセット
 			statement=connection.prepareStatement(baseSql+condition+order);
 			//学校コードをバインド
-			statement.setString(1, student.getSchool().getCd());
+			statement.setString(1, student.getNo());
 			//プリパレの実行
 			rSet=statement.executeQuery();
 			//リストへの格納処理を実行
-
-
+			list=postFilter(rSet);
+	}catch (Exception e) {
+		throw e;
+	}finally {
+		// プリペアードステートメントを閉じる
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (SQLException sqle) {
+				throw sqle;
+			}
+		}
+		// コネクションを閉じる
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException sqle) {
+				throw sqle;
+			}
+		}
+	}
+	return list;
 	}
 }
