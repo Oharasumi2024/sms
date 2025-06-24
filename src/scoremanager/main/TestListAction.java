07cb95c0ed96ee1712abe91f1175c8c600eecd0b
Oharasumi <ymg2465034@stu.o-hara.ac.jp>
2025-06-24 13:55:37 +0900

    角田作業完了

--- src/scoremanager/main/TestListAction.java
+++ src/scoremanager/main/TestListAction.java
@@ -2,19 +2,15 @@ package scoremanager.main;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bean.Student;
 import bean.Subject;
 import bean.Teacher;
 import dao.ClassNumDao;
-import dao.StudentDao;
 import dao.SubjectDao;
 import tool.Action;
 
@@ -32,14 +28,10 @@ public class TestListAction extends Action {
 		String subject = ""; // 入力された科目
 		int entYear = 0; // 入学年度
 		String student_no="";//入力された学生番号
-		List<Student> students = null; // 学生リスト
 		LocalDate todaysDate = LocalDate.now(); // LocalDateインスタンスを取得
 		int year = todaysDate.getYear(); // 現在の年を取得
-		StudentDao studentDao = new StudentDao(); // 学生Dao
 		ClassNumDao classNumDao = new ClassNumDao(); // クラス番号Daoを初期化
 		SubjectDao subjectDao=new SubjectDao();//科目DAOを初期化
-		Map<String, String> errors = new HashMap<>(); // エラーメッセージ
-		boolean isAttend=false;//在学フラグ
 
 		//リクエストパラメーターの取得
 		entYearStr=req.getParameter("f1");
