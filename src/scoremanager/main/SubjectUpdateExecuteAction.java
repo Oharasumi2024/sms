61049da7a9837a1189ea055e8210b04f3d232cbb
Paramin0718 <ymg2465024@stu.o-hara.ac.jp>
2025-06-30 11:47:51 +0900

    totyuu

--- src/scoremanager/main/SubjectUpdateExecuteAction.java
+++ src/scoremanager/main/SubjectUpdateExecuteAction.java
@@ -31,23 +31,30 @@ public class SubjectUpdateExecuteAction extends Action {
 
 
         // 科目の存在チェック
-        	if (errors.isEmpty()) {
-            Subject existingSubject = subjectDao.get(cd, teacher.getSchool());
-            if (existingSubject == null) {
-                errors.put("cd", "科目コードは存在しません");
+        //	if (errors.isEmpty()) {
+           // Subject existingSubject = subjectDao.get(cd, teacher.getSchool());
+            if (subjectDao.get(cd, teacher.getSchool()) == null) {
+                errors.put("1", "科目が存在しません");
+                req.setAttribute("errors", errors);
+            }else{
+                // 科目情報を更新
+                Subject subject = new Subject();
+                subject.setCd(cd);
+                subject.setName(name);
+                subject.setSchool(teacher.getSchool());
+
+                subjectDao.save(subject);
             }
-        }
 
+            req.setAttribute("cd", cd);
+            req.setAttribute("name", name);
 
-        // 科目情報を更新
-        Subject subject = new Subject();
-        subject.setCd(cd);
-        subject.setName(name);
-        subject.setSchool(teacher.getSchool());
-
-        subjectDao.save(subject);
 
         // 完了画面へ遷移
+        	if (errors.isEmpty()){
         req.getRequestDispatcher("subject_update_done.jsp").forward(req, res);
+        	}else{
+        		req.getRequestDispatcher("SubjectUpdate.action").forward(req, res);
+        	}
     }
 }
