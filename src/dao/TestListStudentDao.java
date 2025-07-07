d0a15ce35f7f048e7929cbda222b42a94fa16dd7
Oharasumi <ymg2465034@stu.o-hara.ac.jp>
2025-07-07 10:59:46 +0900

    修正しました

--- src/dao/TestListStudentDao.java
+++ src/dao/TestListStudentDao.java
@@ -12,7 +12,7 @@ import bean.TestListStudent;
 
 public class TestListStudentDao extends Dao {
 
-	private String baseSql="select * from test where ";
+	private String baseSql="select subject.name, test.subject_cd, test.no, test.point from test";
 
 	private List<TestListStudent> postFilter(ResultSet rSet)throws Exception{
 		// リストを初期化
@@ -23,6 +23,7 @@ public class TestListStudentDao extends Dao {
 				// 学生リストインスタンスを初期化
 				TestListStudent testliststudent = new TestListStudent();
 				// 学生リストインスタンスに検索結果をセット
+				testliststudent.setSubjectName(rSet.getString("name"));
 				testliststudent.setSubjectCd(rSet.getString("subject_cd"));
 				testliststudent.setNum(rSet.getInt("no"));
 				testliststudent.setPoint(rSet.getInt("point"));
@@ -44,15 +45,18 @@ public class TestListStudentDao extends Dao {
 		PreparedStatement statement=null;
 		//リザルトセット
 		ResultSet rSet=null;
+		//join
+		String join =" join subject on test.subject_cd = subject.cd";
 		//条件
-		String condition=" student_no=? ";
+		String condition=" where test.student_no = ? and test.school_cd = ?";
 		//ソート
-		String order=" order by subject_cd";
+		String order=" order by subject_cd asc , no asc";
 		try {
 			//プリパレにステートメントをセット
-			statement=connection.prepareStatement(baseSql+condition+order);
+			statement=connection.prepareStatement(baseSql+join+condition+order);
 			//学校コードをバインド
 			statement.setString(1, student.getNo());
+			statement.setString(2, student.getSchool().getCd());
 			//プリパレの実行
 			rSet=statement.executeQuery();
 			//リストへの格納処理を実行
